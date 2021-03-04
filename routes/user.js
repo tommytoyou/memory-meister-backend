5d1f3cf3354fb554acf98cba55ffa243a5db42ca
Rome Bell <romebell5@gmail.com>
2021-03-04 14:54:18 -0800

    controllers: add login and /login route

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const ctrl = require('../controllers');
 
 router.get('/test', ctrl.user.test);
 router.post('/register', ctrl.user.register);
+router.post('/login', ctrl.user.login);
 
 module.exports = router;
