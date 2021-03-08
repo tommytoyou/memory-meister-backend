785a76071be38af5d7dd8a58bee3fb16094e176a
Rome Bell <romebell5@gmail.com>
2021-03-07 20:14:04 -0800

    seed: add message seeding

--- seed.js
+++ seed.js
@@ -6,6 +6,7 @@ const examples = [
 ];
 const oneExample = { name: 'Example 3', completed: true };
 
+
 const addManyExamples = async () => {
   const savedExamples = await db.Example.insertMany(examples);
   console.log('=======> Saved Examples.');
@@ -19,5 +20,16 @@ const addOneExample = async () => {
 }
 
 // run the functions
-addManyExamples();
-addOneExample();
+// addManyExamples();
+// addOneExample();
+
+const oneMessage = { content: 'This is the first message' };
+
+const addOneMessage = async () => {
+  const savedOneMessage = await db.Message.create(oneMessage);
+  console.log('=======> Saved One Message.');
+  console.log(savedOneMessage);
+}
+
+// run the function
+addOneMessage();
