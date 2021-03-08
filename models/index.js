8ffd721366c64b2e870f9aed7efefff1f978e337
Rome Bell <romebell5@gmail.com>
2021-03-07 20:21:04 -0800

    message: import/export from index file in models, controllers, routes

--- models/index.js
+++ models/index.js
@@ -15,5 +15,6 @@ mongoose.connect(MONGO_URL, configOptions)
 
 module.exports = {
     Example: require('./example'),
-    User: require('./user')
+    User: require('./user'),
+    Message: require('./message'),
 };
