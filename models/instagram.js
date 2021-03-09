8c2a7e2177cc640f8d94c3fd2e33a3a49de60f08
Rome Bell <romebell5@gmail.com>
2021-03-09 09:55:54 -0800

    instagram: add model

---
+++ models/instagram.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const instagramPostSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        default: 1
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    comments: [commentSchema],
+});
+
+const InstagramPost = mongoose.model('InstagramPost', instagramPostSchema);
+
+module.exports = InstagramPost;
